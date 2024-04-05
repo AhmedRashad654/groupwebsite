99896543be0d4cc1521fbfdaf7fade5447260f32
AbdulrahmanWork <abdulrahmanwork1999@gmail.com>
2024-04-05 21:04:41 +0200

    new changes

---
+++ src/componantUser/ArchiefThouraUser/ArchiefThourahUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArchiefThourahUser() {
+  return (
+    <div>ArchiefThourahUser</div>
+  )
+}
---
+++ src/componantUser/GraamQasad/GaraamQasad.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GaraamQasad() {
+  return (
+    <div>GaraamQasad</div>
+  )
+}
